b9d33cb44ffc0a9821709c28c270949a16046d90
Dorian1995 <dorianbanica2395@gmail.com>
2022-11-14 17:11:19 +0000

    my local repository

---
+++ components/counter1.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+export default class Counter extends Component {
+    render() {
+        return (<div><h1>Counter App</h1> <button>Increment</button> <button>Decrement</button>
+        </div>);
+    }
+}
+
+state = { count: 0 };
+render()
+return (<div><button>Increment</button> <span>{this.state.count}</span> <button>Decrement</button>
+</div>);
+
+<span>{this.formatCount()}</span>
+formatCount()
+const { count } = this.state;
+return count === 0 ? 'Zero' : count;
+
+state = {
+    count: 0,
+    imageUrl: 'https://picsum.photos/200'
+};
+<><img src={this.state.imageUrl} />
+
+    <button className="btn btn-dark btn-sm"> +
+    </button><span className="badge bg-info m-2"> {this.formatCount()}
+    </span><button className="btn btn-dark btn-sm"> -
+    </button></>
+
+    
+
+
