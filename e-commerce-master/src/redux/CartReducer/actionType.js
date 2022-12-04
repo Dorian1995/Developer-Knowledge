f5a56294e435efc8ef832d7a884ed46e223d35ab
Dorian1995 <92944289+Dorian1995@users.noreply.github.com>
2022-12-04 12:43:33 +0000

    Delete e-commerce-master directory

--- e-commerce-master/src/redux/CartReducer/actionType.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const IN_QTY = "IN_QTY";
-export const DEC_QTY = "DEC_QTY";
-export const REMOVE_QTY = "REMOVE_QTY";
-export const CLEAR_QTY = "CLEAR_QTY";
